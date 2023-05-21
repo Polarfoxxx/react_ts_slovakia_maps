5ba241a87485d58c7863c1b89526287fe91d0c4d
polar_foxx <suchovsky.michal@gmail.com>
2023-05-21 09:12:52 +0200

    storyBook

--- src/module/Table/Components/Table.tsx
+++ src/module/Table/Components/Table.tsx
@@ -76,6 +76,7 @@ const columns = [
                         >
                             {row.getVisibleCells().map(cell => (
                                 <td
+                                
                                 key={cell.id}>
                                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                 </td>
